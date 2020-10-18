d92a9640c719e74c8f5e4f1dac7f0bee50257568
Alison Ferreira <alison.ferreira.82@gmail.com>
2020-10-18 18:58:36 -0300

    Adjustment in error handling for Java-Based Migration.

--- src/main/java/db/migration/V2__CSV_Unload.java
+++ src/main/java/db/migration/V2__CSV_Unload.java
@@ -30,7 +30,7 @@ public class V2__CSV_Unload extends BaseJavaMigration {
 	private static final String INSERT_MOVIE_NOMINATION = "INSERT INTO movie_nomination (year, title, studios, producers, winner) VALUES (?, ?, ?, ?, ?)";
 	private static final String FINAL_MESSAGE = "CSV migration finished with %d/%d rows migrated.";
 	
-	private static int csvRows = 0;
+	private static int csvCountRows = 0;
 	private static int csvMigratedRows = 0;
 	
 
@@ -40,11 +40,12 @@ public class V2__CSV_Unload extends BaseJavaMigration {
 		
 		Iterable<CSVRecord> csvRecordList = CSVFormat.EXCEL.withHeader().withDelimiter(CSV_DELIMITER).parse(csvFile);
 		
-		for(CSVRecord csvRecord: csvRecordList) {
-			MovieNominationVO movieNomination = getMovieNominationFromCSVRecord(csvRecord);
-			csvRows++;
+		for(CSVRecord csvRecord : csvRecordList) {
+			csvCountRows++;
 			
             try (PreparedStatement ps = context.getConnection().prepareStatement(INSERT_MOVIE_NOMINATION)) {
+            	MovieNominationVO movieNomination = getMovieNominationFromCSVRecord(csvRecord);
+            	
             	int idx = 1;
             	ps.setInt(idx++, movieNomination.getYear());
             	ps.setString(idx++, movieNomination.getTitle());
@@ -52,26 +53,27 @@ public class V2__CSV_Unload extends BaseJavaMigration {
             	ps.setString(idx++, movieNomination.getProducers());
             	ps.setBoolean(idx++, movieNomination.getWinner());
             	
-            	boolean sucess = ps.execute();
-            	
-            	if (sucess) {
-            		csvMigratedRows++;
-            	}
-            }
+            	ps.executeUpdate();
+        		csvMigratedRows++;
+            } catch (Exception e) {
+				log.warn(String.format("%s: %s. In the CSV file line: %d.", e.getClass(), e.getMessage(), (csvCountRows + 1)));
+			}
 		}
 		
 		context.getConnection().commit();
 		
-		log.info(FINAL_MESSAGE.formatted(csvMigratedRows, csvRows));
+		log.info(FINAL_MESSAGE.formatted(csvMigratedRows, csvCountRows));
 	}
 
 	private MovieNominationVO getMovieNominationFromCSVRecord(CSVRecord csvRecord) {
 		MovieNominationVO movieNomination = new MovieNominationVO();
+		
 		movieNomination.setYear(Integer.parseInt(csvRecord.get(CSV_MOVIE_NOMINATION_YEAR)));
 		movieNomination.setTitle(csvRecord.get(CSV_MOVIE_NOMINATION_TITLE));
 		movieNomination.setStudios(csvRecord.get(CSV_MOVIE_NOMINATION_STUDIOS));
 		movieNomination.setProducers(csvRecord.get(CSV_MOVIE_NOMINATION_PRODUCERS));
 		movieNomination.setWinner(CSV_TRUE_VALUE.equalsIgnoreCase(csvRecord.get(CSV_MOVIE_NOMINATION_WINNER)) ? true : false);
+		
 		return movieNomination;
 	}
 
