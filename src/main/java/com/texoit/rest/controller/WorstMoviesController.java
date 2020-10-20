a853e2172f34ba9dc188bb0330ae04aad17292a5
Alison Ferreira <alison.ferreira.82@gmail.com>
2020-10-19 22:06:48 -0300

    Logic improvements taking advantage of Java 8 functional programming.

--- src/main/java/com/texoit/rest/controller/WorstMoviesController.java
+++ src/main/java/com/texoit/rest/controller/WorstMoviesController.java
@@ -1,9 +1,9 @@
 package com.texoit.rest.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -25,47 +25,25 @@ public class WorstMoviesController {
 	public Map<String, List<WorstProducersWinnersVO>> getWorstMoviesByMinAndMaxIntervals() {
 		List<WorstProducersWinnersVO> worstProducersWinnersList = worstProducersWinnersModel.getWorstProducersWinnersList();
 		
-		Integer minInterval = null;
-		Integer maxInterval = null;
+		// minInterval operations
+		Integer minInterval = worstProducersWinnersList.stream()
+				.filter(p -> p.getInterval() > 0)
+				.mapToInt(p -> p.getInterval())
+				.min().orElse(Integer.MIN_VALUE);
 		
-		List<WorstProducersWinnersVO> minWorstProducersWinnersList = new ArrayList<WorstProducersWinnersVO>();
-		List<WorstProducersWinnersVO> maxWorstProducersWinnersList = new ArrayList<WorstProducersWinnersVO>();
+		List<WorstProducersWinnersVO> minWorstProducersWinnersList = worstProducersWinnersList.stream()
+				.filter(p -> p.getInterval().equals(minInterval)).collect(Collectors.toList());
 		
-		for (WorstProducersWinnersVO worstProducersWinners : worstProducersWinnersList) {
-			int interval = worstProducersWinners.getInterval();
-			
-			if (interval > 0) {
-				// minInterval operations
-				if (minInterval == null) {
-					minInterval = interval;
-				}
-				
-				if (interval < minInterval ) {
-					minInterval = interval;
-					minWorstProducersWinnersList = new ArrayList<WorstProducersWinnersVO>();
-				}
-				
-				if (interval <= minInterval ) {
-					minWorstProducersWinnersList.add(worstProducersWinners);
-				}
-				
-				// maxInterval operations
-				if (maxInterval == null) {
-					maxInterval = interval;
-				}
-				
-				if (interval > maxInterval) {
-					maxInterval = interval;
-					maxWorstProducersWinnersList = new ArrayList<WorstProducersWinnersVO>();
-				}
-				
-				if (interval >= maxInterval ) {
-					maxWorstProducersWinnersList.add(worstProducersWinners);
-				}
-			}
-			
-		}
+		// maxInterval operations
+		Integer maxInterval = worstProducersWinnersList.stream()
+				.filter(p -> p.getInterval() > 0)
+				.mapToInt(p -> p.getInterval())
+				.max().orElse(Integer.MAX_VALUE);
 		
+		List<WorstProducersWinnersVO> maxWorstProducersWinnersList = worstProducersWinnersList.stream()
+				.filter(p -> p.getInterval().equals(maxInterval)).collect(Collectors.toList());
+		
+		// final result mount
 		Map<String, List<WorstProducersWinnersVO>> result = new HashMap<String, List<WorstProducersWinnersVO>>();
 		result.put("min", minWorstProducersWinnersList);
 		result.put("max", maxWorstProducersWinnersList);
